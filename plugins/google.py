777a5d80ca650311747aa9324e2a7741c57f361e
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-06-25 22:15:33 +0530

    Create google.py

---
+++ plugins/google.py
@@ -0,0 +1,56 @@
+@Bot.on_message(filters.private & filters.text)
+async def filter(bot, update):
+    await update.reply_text(
+        text="`Click the button below for searching...`",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="Search Here", switch_inline_query_current_chat=update.text)],
+                [InlineKeyboardButton(text="Search in another chat", switch_inline_query=update.text)]
+            ]
+        ),
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_inline_query()
+async def inline(bot, update):
+    results = google(update.query)
+    answers = []
+    for result in results:
+        answers.append(
+            InlineQueryResultArticle(
+                title=result["title"],
+                description=result["description"],
+                input_message_content=InputTextMessageContent(
+                    message_text=result["text"],
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton(text="Link", url=result["link"])],
+                        JOIN_BUTTON
+                    ]
+                )
+            )
+        )
+    await update.answer(answers)
+
+
+def google(query):
+    r = requests.get(API + requote_uri(query))
+    informations = r.json()["results"][:50]
+    results = []
+    for info in informations:
+        text = f"**Title:** `{info['title']}`"
+        text += f"\n**Description:** `{info['description']}`"
+        text += f"\n\nMade by @FayasNoushad"
+        results.append(
+            {
+                "title": info['title'],
+                "description": info['description'],
+                "text": text,
+                "link": info['link']
+            }
+        )
+    return results
